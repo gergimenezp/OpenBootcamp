0864c0689a8fef7624a95f6ac0d77afabb0864c5
gergimenezp <gergimenez@hotmail.com>
2022-06-07 10:18:22 +0200

    notacion de datos

---
+++ Javascript/02/lista.js
@@ -0,0 +1,12 @@
+const yo = [
+    Gerardo,
+    36,
+    true,
+    new Date(1985, 5, 18),
+    {
+        titulo: "La Caverna",
+        autor: "José Saramago",
+        fecha: new Date(2001, 0, 2),
+        url: "https://www.amazon.es/caverna-BIBLIOTECA-SARAMAGO-Jos%C3%A9-Saramago/dp/8420442283"
+    },
+];
