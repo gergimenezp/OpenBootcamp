4c312141d13b569378f4dccd78bb4e440f9294a2
gergimenezp <gergimenez@hotmail.com>
2022-06-10 20:24:30 +0200

    objetos 2

---
+++ Javascript/07/objetos.js
@@ -0,0 +1,8 @@
+const yo = {nombre: "Gerardo", apellido: "Gimenez Ponce", edad: 36, altura: 1.83, eresDesarrollador: true}
+const edad = yo.edad;
+const amigos = [yo,
+    {nombre: "Juan", apellido: "Perez", edad: 40, altura: 1.85, eresDesarrollador: false},
+    {nombre: "Pedro", apellido: "Ramirez", edad: 29, altura: 1.76, eresDesarrollador: true}
+]
+
+const porEdad = amigos.sort((a, b) => b.edad - a.edad)
