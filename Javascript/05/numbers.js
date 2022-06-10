b23730a3da7417fdb951bf6d8858de09a55edc8e
gergimenezp <gergimenez@hotmail.com>
2022-06-10 08:52:19 +0200

    numbers

---
+++ Javascript/05/numbers.js
@@ -0,0 +1,6 @@
+let alturaCM = 183;
+let alturaM = 1.83;
+let peso = 96.3;
+let alturaRedondeada = Math.ceil(alturaM);
+let pesoRedondeado = Math.floor(peso);
+let f = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
